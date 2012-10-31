85690f22ab3701430cee0ad37a8f657ba12cd864
Marina Martin <git@marinamartin.com>
2012-10-30 23:03:21 -0400

    Updated Readme

--- app/controllers/send_sms_controller.rb
+++ app/controllers/send_sms_controller.rb
@@ -4,8 +4,6 @@ class SendSmsController < ApplicationController
       message_body = params["Body"]
       from_number = params["From"]
 
-      #SMSLogger.log_text_message from_number, message_body
-      
       client = Twilio::REST::Client.new(ENV['twiliosid'], ENV['twiliotoken'])
 
       client.account.sms.messages.create(
