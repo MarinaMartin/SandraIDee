1b7254cd5b0549557e551aea34ae73a3bea9a2a7
Marina Martin <git@marinamartin.com>
2012-10-30 20:49:01 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandraIDee::Application.config.secret_token = '46f37cee255f9afc836b9a76e5df0c1c4788fc03df246a353e9397aebb05a4c13546c36d9fc8cefb0f2fdee4c511928b2f2937998ef592e0e46e2be83a46e582'
