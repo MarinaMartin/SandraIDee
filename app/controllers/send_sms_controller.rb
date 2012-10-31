2c9fdd39d8e16c9f8c1c1ab1623c4c3f6b7c7da6
Marina Martin <git@marinamartin.com>
2012-10-30 22:14:20 -0400

    Trying Twilio endpoint

---
+++ app/controllers/send_sms_controller.rb
@@ -0,0 +1,18 @@
+class SendSmsController < ApplicationController
+  
+  def index 
+      message_body = params["Body"]
+      from_number = params["From"]
+
+      #SMSLogger.log_text_message from_number, message_body
+      
+      client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+
+      client.account.sms.messages.create(
+        :from => TWILIO_CONFIG['from'],
+        :to => from_number,
+        :body => "Message body"
+      )
+      
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SandraIDee::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  match 'sms' => 'sendsms#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -48,7 +48,7 @@ SandraIDee::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # root :to => 'sendsms#index'
 
   # See how all your routes lay out with "rake routes"
 
