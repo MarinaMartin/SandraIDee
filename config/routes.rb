2f828324140edf0db7c0bf69d0480da8fe845e3c
Marina Martin <git@marinamartin.com>
2012-10-30 22:15:06 -0400

    Trying Twilio endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SandraIDee::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-  match 'sms' => 'sendsms#index'
+  match 'sms' => 'send_sms#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
